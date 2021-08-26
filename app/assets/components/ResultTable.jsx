fe880e6452b5c10674fd251feabedafe951bbc06
Marco Picasso <marco.picasso@hotmail.it>
2021-08-26 15:14:33 +0200

    refactor result table

--- app/assets/components/ResultTable.jsx
+++ app/assets/components/ResultTable.jsx
@@ -3,26 +3,23 @@ import { Header, Table } from 'semantic-ui-react'
 
 const ResultTable = (props) => {
   const bestFlights = props.bestFlights[0];
+  console.log('bestfl', bestFlights)
   if (bestFlights) {
     return (
       <Table basic='very' celled collapsing>
       <Table.Header>
         <Table.Row>
+          <Table.HeaderCell>From</Table.HeaderCell>
+          <Table.HeaderCell>To</Table.HeaderCell>
           <Table.HeaderCell>Price</Table.HeaderCell>
-          <Table.HeaderCell>Code Arrival</Table.HeaderCell>
         </Table.Row>
       </Table.Header>
   
       <Table.Body>
         <Table.Row>
-          <Table.Cell>
-            <Header as='h4'>
-              <Header.Content>
-                {props.bestFlights[0].price}
-              </Header.Content>
-            </Header>
-          </Table.Cell>
-          <Table.Cell>{props.bestFlights[0].code_arrival}</Table.Cell>
+          <Table.Cell>{props.from}</Table.Cell>
+          <Table.Cell>{props.to}</Table.Cell>
+          <Table.Cell>{props.bestFlights[0].price}</Table.Cell>
         </Table.Row>
       </Table.Body>
     </Table>
@@ -30,8 +27,6 @@ const ResultTable = (props) => {
   } else {
     return <h3>No flights selected</h3>
   }
-
-
 }
 
 
--- app/assets/components/SearchForm.jsx
+++ app/assets/components/SearchForm.jsx
@@ -3,16 +3,22 @@ import { Button, Divider, Form } from "semantic-ui-react";
 import axios from "axios";
 import ResultTable from "./ResultTable";
 
-const stopoverOptions = [
-  { text: "0", value: 0 },
-  { text: "1", value: 1 },
-  { text: "2", value: 2 },
-];
-
 class SearchForm extends Component {
   constructor() {
     super();
-    this.state = { airports: [], loading: true, from: "", to: "", stopover: "", bestFlights: [] };
+    this.state = {
+      airports: [],
+      loading: true,
+      from: "",
+      to: "",
+      stopover: "",
+      bestFlights: [],
+      stopoverOptions: [
+        { text: "0", value: 0 },
+        { text: "1", value: 1 },
+        { text: "2", value: 2 },
+      ]
+    };
   }
 
   componentDidMount() {
@@ -26,60 +32,83 @@ class SearchForm extends Component {
   }
 
   getBestPrice() {
-    const { from, to, stopover } = this.state
-    axios.get(`http://localhost/api/bestflights`, null, { params: {
-      from,
-      to,
-      stopover
-    }}).then((bestFlights) => {
-      this.setState({ bestFlights: bestFlights.data, loading: false });
-    });
-    console.log('best', this.state.bestFlights)
+    const { from, to, stopover } = this.state;
+    axios
+      .get(`http://localhost/api/bestflights`, null, {
+        params: {
+          from,
+          to,
+          stopover,
+        },
+      })
+      .then((bestFlights) => {
+        this.setState({ bestFlights: bestFlights.data });
+      });
   }
 
-  handleChange = (name, value) => {
-    this.setState({ [name]: value })
-    console.log(name)
-  }
+  handleDropdownChange = (name, value, e) => {
+    this.setState({ [name]: value });
 
-  handleSubmit = () => {
-    this.getBestPrice()
-  }
+    console.log(e.target.innerText);
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.getBestPrice();
+  };
+
+  resetSearch = () =>
+    this.setState({ from: "", to: "", stopover: "", bestFlights: [] });
 
   render() {
-    const loading = this.state.loading;
+    const { airports, from, to, stopover, bestFlights, loading, stopoverOptions } = this.state
     return (
       <>
         <Form onSubmit={this.handleSubmit}>
-            <Form.Select
-              name="from"
-              fluid
-              label="Departure Airport"
-              options={this.state.airports}
-              placeholder="Select an airport"
-              onChange={(e, { value, name }) => this.handleChange(name, value)}
-            />
-            <Form.Select
-              name="to"
-              fluid
-              label="Arrival Airport"
-              options={this.state.airports}
-              placeholder="Select an airport"
-              onChange={(e, { value, name }) => this.handleChange(name, value)}
-            />
-            <Form.Select
-            name="stopovers"
+          <Form.Select
+            loading={loading}
+            value={from}
+            name="from"
+            fluid
+            label="Departure Airport"
+            options={airports}
+            placeholder="Select an airport"
+            onChange={(e, { value, name }) => this.handleDropdownChange(name, value, e)}
+          />
+          <Form.Select
+            loading={loading}
+            value={to}
+            name="to"
+            fluid
+            label="Arrival Airport"
+            options={airports}
+            placeholder="Select an airport"
+            onChange={(e, { value, name }) => this.handleDropdownChange(name, value)}
+          />
+          <Form.Select
+            value={stopover}
             name="stopover"
-              fluid
-              label="Stopovers"
-              options={stopoverOptions}
-              placeholder="Select a value"
-              onChange={(e, { value, name }) => this.handleChange(name, value)}
-            />
-          <Button type="submit">Search</Button>
+            fluid
+            label="Stopovers"
+            options={stopoverOptions}
+            placeholder="Select a value"
+            onChange={(e, { value, name }) => this.handleDropdownChange(name, value)}
+          />
+          <Button 
+            color="green" 
+            type="submit" 
+            disabled={!from
+            || !to
+            }
+            >Search</Button>
+          <Button  color="pink"  onClick={this.resetSearch}>Reset</Button>
         </Form>
         <Divider />
-        <ResultTable bestFlights={this.state.bestFlights} />
+        <ResultTable 
+          bestFlights={bestFlights} 
+          from={from} 
+          to={to} 
+          />
       </>
     );
   }
